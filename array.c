0c6f847aa51bcac9b16afe8fadf4b31ebca854c6
gulshan <mgulshan2003@gmail.com>
2025-01-03 19:58:24 +0530

    + array[20], modified welcomeScreen() to capture user input.

--- array.c
+++ array.c
@@ -6,6 +6,9 @@
 
 void welcomeScreen();
 void screenCleaner();
+void userInput(int );
+
+int array[20] = {0};
 
 void main() {
     welcomeScreen();
@@ -13,6 +16,9 @@ void main() {
 
 void welcomeScreen() {
 
+    int choice;
+
+label1:
     screenCleaner();
     
     printf("-- Demonstrating array as abstract data type.\n\n");
@@ -22,10 +28,41 @@ void welcomeScreen() {
     printf("> 2. Deletion.\n");
     printf("> 3. Search an element\n");
     printf("> 4. Reverse the array.\n");
-    printf("> 5. Min/max values.\n\n");
+    printf("> 5. Min/max values.\n");
+    printf("> 6. Exit the program.\n\n");
 
     printf("*NOTE: Before choosing options like deletion, searching,\n"
-           "reverse the array, must insert enough elements.");
+           "reverse the array, must insert enough elements.\n\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1: 
+            userInput(1);
+            break;
+        case 2: 
+            userInput(2);
+            break;
+        case 3:
+            userInput(3);
+            break;
+        case 4:
+            userInput(4);
+            break;
+        case 5:
+            userInput(5);
+            break;
+        case 6:
+            exit(0);
+            break;
+        default:
+            printf("Error! choose from the given options.\n");
+            printf("Press any key to exit...");
+            getch();
+            goto label1;
+
+    } 
 
 }
 
