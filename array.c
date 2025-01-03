6f5fada1b366227164562ccfe0d91f1559fe721a
gulshan <mgulshan2003@gmail.com>
2025-01-03 19:23:07 +0530

    Screen cleaner function added.

--- array.c
+++ array.c
@@ -5,22 +5,31 @@
 #include <stdlib.h>
 
 void welcomeScreen();
+void screenCleaner();
 
 void main() {
     welcomeScreen();
 }
 
 void welcomeScreen() {
+
+    screenCleaner();
     
     printf("-- Demonstrating array as abstract data type.\n\n");
 
     printf("Read the options carefully and choose.\n");
     printf("> 1. Insertion.\n");
     printf("> 2. Deletion.\n");
-    printf("> 3. Search an elementn");
+    printf("> 3. Search an element\n");
     printf("> 4. Reverse the array.\n");
     printf("> 5. Min/max values.\n\n");
 
-    printf("*NOTE: Before choosing options like deletion, searching,\nreverse the array, must insert enough elements.");
+    printf("*NOTE: Before choosing options like deletion, searching,\n"
+           "reverse the array, must insert enough elements.");
 
+}
+
+void screenCleaner() {
+    system("cls");
+    fflush(stdin);
 }
