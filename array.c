2326e5f3712149758b884ae947a4a062dff1691d
gulshan <mgulshan2003@gmail.com>
2025-01-03 19:13:28 +0530

    welcomeScreen() added, with menus.

--- array.c
+++ array.c
@@ -3,3 +3,24 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+
+void welcomeScreen();
+
+void main() {
+    welcomeScreen();
+}
+
+void welcomeScreen() {
+    
+    printf("-- Demonstrating array as abstract data type.\n\n");
+
+    printf("Read the options carefully and choose.\n");
+    printf("> 1. Insertion.\n");
+    printf("> 2. Deletion.\n");
+    printf("> 3. Search an elementn");
+    printf("> 4. Reverse the array.\n");
+    printf("> 5. Min/max values.\n\n");
+
+    printf("*NOTE: Before choosing options like deletion, searching,\nreverse the array, must insert enough elements.");
+
+}
