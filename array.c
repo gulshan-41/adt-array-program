5accf94ee84bd8d47459950d10b9dff390107845
gulshan <mgulshan2003@gmail.com>
2025-01-06 11:23:00 +0530

    +tryAgain()

--- array.c
+++ array.c
@@ -6,13 +6,20 @@
 
 void welcomeScreen();
 void screenCleaner();
+void tryAgain();
+
 void insertion();
 void atBeginning();
+
 void printA();
 int isArrayFull();
+int isArrayEmpty();
 
-int array[20] = {0};
-int counter = 0;
+#define n 20
+
+int array[n] = {0};
+int head = 0;
+int tail = -1;
 
 void main() {
     welcomeScreen();
@@ -45,18 +52,18 @@ label1:
         case 1: 
             insertion();
             break;
-        case 2: 
-            deletion();
-            break;
-        case 3:
-            search();
-            break;
-        case 4:
-            reverse();
-            break;
-        case 5:
-            min_max();
-            break;
+        // case 2: 
+        //     deletion();
+        //     break;
+        // case 3:
+        //     search();
+        //     break;
+        // case 4:
+        //     reverse();
+        //     break;
+        // case 5:
+        //     min_max();
+        //     break;
         case 6:
             exit(0);
             break;
@@ -70,26 +77,52 @@ label1:
 
 }
 
+int isArrayFull() {
+    if(tail >= n - 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int isArrayEmpty() {
+    if(tail == -1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void printA() {
+    int i;
+    
+    for(i = head; i <= tail; i++) {
+        printf(" %d |", array[i]);
+    }
+    printf("\n");
+}
+
 void insertion() {
 
-    int flag, choice1;
+    int choice1;
 
     screenCleaner();
-    flag = isArrayFull();
 
-    if(flag == 1) {
+    if(isArrayFull()) {
         printf("Alert! Array is full.\n");
         printf("Press any key to continue...");
         getch();
         welcomeScreen();
     } 
-
+label2:
+    screenCleaner();
     printf("-- Insertion\n\n");
     printf("Options: \n");
     printf("> 1. At the beginning.\n");
     printf("> 2. At a specific index.\n");
     printf("> 3. At the end.\n");
-    printf("> 4. In a sorted array.\n\n");
+    printf("> 4. In a sorted array.\n");
+    printf("> 5. exit from this section.\n\n");
     printf("Enter your choice: ");
     scanf("%d", &choice1);
 
@@ -97,7 +130,57 @@ void insertion() {
         case 1:
             atBeginning();
             printA();
+            tryAgain();
             break;
+        case 5:
+            welcomeScreen();
+            break;
+        default:
+            printf("\nError! choose from the given options.");
+            printf("\nPress any key to continue...");
+            getch();
+            goto label2;
+    }
+}
+
+void atBeginning() {
+    int data, i;
+
+    printf("\ndata: ");
+    scanf("%d", &data);
+
+    if(isArrayEmpty()) {
+        array[0] = data;
+        tail++;
+    } else {
+        for(i = tail; i >= head; i--) {
+            array[i + 1] = array[i];
+        }
+        tail++;
+    }
+    
+}
+
+void tryAgain() {
+    char ch;
+
+    printf("\nDo you want to try again [y/N]: ");
+    scanf(" %c", &ch);
+
+    switch(ch) {
+        case 'y':
+        case 'Y':
+            insertion();
+            break;
+        case 'n':
+        case 'N':
+            welcomeScreen();
+            break;
+        default:
+            printf("\nError: invalid input. \n");
+            printf("Press any key to continue... \n");
+            getch();
+            welcomeScreen();
     }
 }
 
